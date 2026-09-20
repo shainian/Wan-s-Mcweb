8ecd414fc1e86548df670c8a6f10919999afffea
shainian <160017248+shainian@users.noreply.github.com>
2026-09-20 21:27:11 +0800

    Update website configuration details

--- src/config/website.config.js
+++ src/config/website.config.js
@@ -2,10 +2,10 @@
 export const websiteConfig = {
   // 网站基本信息
   site: {
-    title: "晚晚服",
-    description: "欢迎来到晚晚服，探索无限可能的方块世界！",
+    title: "青茗屿",
+    description: "欢迎来到青茗屿，原版生存玩法！",
     keywords: "Minecraft,服务器,我的世界,Java版,生存,创造,生电",
-    author: "晚晚服",
+    author: "青茗屿",
     language: "zh-CN"
   },
 
@@ -197,17 +197,17 @@ export const websiteConfig = {
   // 服务器配置
   server: {
     // 页面显示的服务器地址
-    displayAddress: "w-6.cc",
+    displayAddress: "mc008.cn",
     // API请求使用的服务器地址（可以不同）
-    apiAddress: "ipv4.w-6.cc",
+    apiAddress: "mc008.cn",
     port: "25565",
-    version: "1.21.4",
+    version: "1.21.11",
     apiEndpoint: "https://api.mcstatus.io/v2/status/java",
   },
 
   // 页脚配置
   footer: {
-    copyright: "© 2022-2026 晚晚服保留所有权利.",
+    copyright: "© 2022-2026 青茗屿保留所有权利.",
     customHtml: `
       <!-- 在这里插入自定义HTML代码，比如备案信息 -->
       <div style='display: flex; justify-content: center; align-items: center; width: 100%;'><a target='_blank' href='https://beian.miit.gov.cn/'><img src='/foot/foot-icp.svg' style='display: block;'></a></div>

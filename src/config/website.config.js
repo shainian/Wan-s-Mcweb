373c99915c768885207b7b75f78a4704eb52eec9
shainian <160017248+shainian@users.noreply.github.com>
2026-09-20 23:19:00 +0800

    Update website.config.js

--- src/config/website.config.js
+++ src/config/website.config.js
@@ -130,13 +130,8 @@ export const websiteConfig = {
   // 背景图片配置
   background: {
     images: [
-      "/backgrounds/desktopWallpaper_1.webp",
       "/backgrounds/desktopWallpaper_2.webp",
       "/backgrounds/desktopWallpaper_3.webp",
-      "/backgrounds/desktopWallpaper_4.webp",
-      "/backgrounds/desktopWallpaper_5.webp",
-      "/backgrounds/desktopWallpaper_6.webp",
-      "/backgrounds/desktopWallpaper_7.webp",
     ],
     defaultImage: "/backgrounds/default.webp"
   },
